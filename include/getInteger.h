f53f7b66bcc08a37571fa4f577041421916f1863
Gavrilov Artyom <rzdartem13@gmail.com>
2023-11-18 02:40:58 +0300

    refactor

--- include/getInteger.h
+++ include/getInteger.h
@@ -1,3 +1,9 @@
 #pragma once
 
-long getInteger(const char *);
+/*! \brief
+ * Gets integer from stdin.
+ * @param outputString Output that will be displayed until an integer is entered
+ * @return
+ * Returns integer getting from stdin
+ */
+long getInteger(const char *outputString);
--- src/getInteger.c
+++ src/getInteger.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int checkStrtolErrors(const char *endptr, const char *buf, const long number) {
-    if (errno == ERANGE) {
-        perror("the number is too small or too large.");
+int checkErr(const char *endptr, const char *buf, const long number) {
+    if (errno == ERANGE)
+    {
+        puts("the number is too small or too large.");
         return 0;
-    } else if (endptr == buf) {
-        perror("the number isn't the integer.");
+    }
+    else if (endptr == buf)
+    {
+        puts("the number isn't the integer.");
         return 0;
-    } else if (*endptr && *endptr != '\n') {
-        perror("wrong symbols were entered. "
+    }
+    else if (*endptr && *endptr != '\n')
+    {
+        puts("wrong symbols were entered. "
                "Fractional number or odd space after the number may have been entered.");
         return 0;
-    } else if (errno != 0 && number == 0) {
-        perror("unspecified error occurred.");
+    }
+    else if (errno != 0 && number == 0)
+    {
+        puts("unspecified error occurred.");
         return 0;
     }
     return 1;
 }
 
-long getInteger(const char *string)
+long getInteger(const char *outputString)
 {
-    char buf[1024]; //use 1KiB just to be sure
+    char buf[1024];
     int successFlag;
     long number;
-    do{
-        printf("%s\n", string);
-        if (!fgets(buf, 1024, stdin)){
+    do
+    {
+        printf("%s\n", outputString);
+        if (!fgets(buf, 1024, stdin))
+        {
             puts("reading input error.");
             exit(1);
         }
@@ -34,7 +43,7 @@ long getInteger(const char *string)
         char *endptr = NULL;
         errno = 0;
         number = strtol(buf, &endptr, 10);
-        successFlag = checkStrtolErrors(endptr, buf, number);
+        successFlag = checkErr(endptr, buf, number);
     } while (!successFlag);
     return number;
 }
